2006bf1a63d546f19365930de92199f73efe23b9
swetha <swetha.s@razorthink.net>
2016-11-16 19:02:11 +0530

    last commit on[16-11-2016]

--- my_class.java
+++ my_class.java
@@ -6,5 +6,6 @@ package my_pack;
 public class my_class {
     public static void main(String[] args) {
         System.out.println("hello world. This is my first project");
+        System.out.println("welcome to java programing");
     }
 }
