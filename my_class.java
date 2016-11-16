e191906870a57418a69305f963112080a05114ea
swetha <swetha.s@razorthink.net>
2016-11-16 16:50:49 +0530

    created a java file

---
+++ my_class.java
@@ -0,0 +1,10 @@
+package my_pack;
+
+/**
+ * Created by swetha on 16/11/16.
+ */
+public class my_class {
+    public static void main(String[] args) {
+        System.out.println("hello world. This is my first project");
+    }
+}
